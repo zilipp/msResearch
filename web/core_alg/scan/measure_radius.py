5f43963d6d9aabe43919a87a971ae956cbdff448
Zi Li <zili100097@gmail.com>
2020-10-06 13:56:27 -0700

    add/modify visualization for tibia/radius

--- web/core_alg/scan/measure_radius.py
+++ web/core_alg/scan/measure_radius.py
@@ -15,14 +15,38 @@ rml_coeff = 0.996
 rmld_coeff = 1.035
 
 
-def get_rml(alpha_shape):
+def get_rml(alpha_shape, show_figure, left_bone_points_ordered, right_bone_points_ordered):
     (min_x, min_y, max_x, max_y) = alpha_shape.exterior.bounds
     rml = max_x - min_x
+    if show_figure:
+        # most left point, 1st POIs
+        p_left = []
+        for i in range(len(left_bone_points_ordered)):
+            if left_bone_points_ordered[i][0] == min_x:
+                p_left = left_bone_points_ordered[i]
+                break
+
+        # most right point, 2nd POIs
+        p_right = []
+        for i in range(len(right_bone_points_ordered)):
+            if right_bone_points_ordered[i][0] == max_x:
+                p_right = right_bone_points_ordered[i]
+                break
+
+        fig, ax = plt.subplots()
+        x, y = alpha_shape.exterior.xy
+        ax.plot(x, y)
+        ax.plot(p_left[0], p_left[1], 'r+')
+        ax.plot(p_right[0], p_right[1], 'r+')
+        ax.set_aspect('equal')
+        plt.show()
+
+
     rml /= rml_coeff
     return rml
 
 
-def get_rmld(center_bone_points, show_figure):
+def get_rmld(center_bone_points, show_figure, alpha_shape):
     center_bone_points = np.asarray(center_bone_points)
     center_bone_points_upper = np.asarray(
         [x for x in center_bone_points if x[1] >= 0])
@@ -44,7 +68,7 @@ def get_rmld(center_bone_points, show_figure):
         a = top_line_p[2] * x * x + top_line_p[1] * x + top_line_p[0]
         b = bottom_line_p[2] * x * x + bottom_line_p[1] * x + bottom_line_p[0]
         plt.plot(x, a, 'r')  # plotting t, a separately
-        plt.plot(x, b, 'b')  # plotting t, b separately
+        plt.plot(x, b, 'r')  # plotting t, b separately
 
         plt.scatter(
             center_bone_points_upper[:, 0], center_bone_points_upper[:, 1])
@@ -57,6 +81,19 @@ def get_rmld(center_bone_points, show_figure):
             center_bone_points_lower[:, 0], center_bone_points_lower[:, 1],  marker='+')
 
         plt.plot([0], [0], '*')  # plotting t, c separately
+        plt.show()
+
+    if show_figure:
+        fig, ax = plt.subplots()
+        x, y = alpha_shape.exterior.xy
+        ax.plot(x, y)
+        x = np.linspace(-25, 25, num=100)
+        a = top_line_p[2] * x * x + top_line_p[1] * x + top_line_p[0]
+        b = bottom_line_p[2] * x * x + bottom_line_p[1] * x + bottom_line_p[0]
+        ax.plot(x, a, 'r')  # plotting t, a separately
+        ax.plot(x, b, 'r')  # plotting t, b separately
+        ax.plot([0], [0], 'r*')  # plotting t, c separately
+        ax.set_aspect('equal')
         plt.show()
 
     # vertical line
@@ -89,8 +126,12 @@ def get_rmld(center_bone_points, show_figure):
 def get_measurement(radius, show_figure):
     logging.info('Start measuring radius...')
 
+    _, left_region_points_ordered = bone_region_util.get_left_region(
+        radius.alpha_shape)
     center_region, center_region_points = bone_region_util.get_center_region(
         radius.alpha_shape)
+    _, right_region_points_ordered = bone_region_util.get_right_region(
+        radius.alpha_shape)
 
-    radius.rml = get_rml(radius.alpha_shape)
-    radius.rmld = get_rmld(center_region_points, show_figure)
+    radius.rml = get_rml(radius.alpha_shape, show_figure, left_region_points_ordered, right_region_points_ordered)
+    radius.rmld = get_rmld(center_region_points, show_figure, radius.alpha_shape)
